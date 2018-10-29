a66024509d742c0846813c45dd6ddeb1b8af2927
amit silber <amitsilber@gmail.com>
2018-10-29 15:01:35 +0200

    ChooseIngredients: add icons and button.

--- app/src/main/java/com/huji/foodtricks/foodtricks/ImageAdapter.java
+++ app/src/main/java/com/huji/foodtricks/foodtricks/ImageAdapter.java
@@ -15,17 +15,13 @@ public class ImageAdapter extends BaseAdapter {
     private Context mContext;
     // references to our images
     private Integer[] mThumbIds = {
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7,
-            R.drawable.sample_0, R.drawable.sample_1,
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7,
-            R.drawable.sample_0, R.drawable.sample_1,
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7
+            R.drawable.apple, R.drawable.avocado,
+            R.drawable.broccoli, R.drawable.carrot,
+            R.drawable.cheese, R.drawable.chili,
+            R.drawable.fish, R.drawable.flour,
+            R.drawable.lemon, R.drawable.meat,
+            R.drawable.mushroom, R.drawable.onion,
+            R.drawable.pasta, R.drawable.salad,
     };
 
     private boolean[] isPressed = new boolean[22];
